1fd3383d3c1e8dcad17f97b170e8f7adc177b711
Whoin Lee <whoinlee@gmail.com>
2023-04-27 23:24:16 -0400

    clean up

--- src/components/util/useEventListener.ts
+++ src/components/util/useEventListener.ts
@@ -1,14 +1,7 @@
 import { useEffect, useRef } from 'react';
 
 
-// type Props = {
-//     eventName:string;
-//     handler:() => void;
-//     element:string;
-// };
-
 const useEventListener = (eventName:string, handler:(e:Event)=>void, element = window) => {
-    // const savedHandler = useRef();
     const savedHandler = useRef<any>();
 
     //-- set the handler as the current value of the ref to cache it
